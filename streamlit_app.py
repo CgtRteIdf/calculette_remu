e02e02e6efecff1f77d7cdace27fedc43acdbc22
CgtRteIdf <95100859+CgtRteIdf@users.noreply.github.com>
2022-01-27 18:03:04 +0100

    mise en gras

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ pd.options.plotting.backend = "plotly"
 st.set_page_config(layout="wide")
 
 st.title('Calculette rémunération')
-st.write('Bienvenue ! Cette calculette permet de projeter sa rémunération actuelle (**brute**) dans le temps, d’intégrer les évolutions prévues (prise de NR par ex) et de comparer cette projection avec l’inflation. **L\'application n\'enregistre et n\'envoie aucune donnée personnelle.**')
+st.write('Bienvenue ! Cette calculette permet de projeter sa rémunération actuelle (**brute**) dans le temps, de **simuler les évolutions prévues** (prise de NR par ex) et de **comparer cette projection avec l’inflation**. **L\'application n\'enregistre et n\'envoie aucune donnée personnelle.**')
 st.write('Pour mémoire l’inflation n’est pas le même indice que le pouvoir d’achat (il ne comprend pas les coûts de logement, par exemple), mais il donne un bon premier aperçu de ce que représenterait votre rémunération d’aujourd’hui avec les prix de demain.')
 texte = ['Dans la partie latérale gauche, vous pouvez définir différents paramètres pour vos simulations :\n',
 	 '- **NR :** votre NR actuel\n',
